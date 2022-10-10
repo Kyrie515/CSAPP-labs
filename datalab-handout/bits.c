b391ab99d411e7d134d9018a474e62d66ee0a7f8
ubuntu <ubuntu@localhost.localdomain>
2022-10-10 14:33:09 +0800

    implement the function logicalNeg

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -245,7 +245,8 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  //for every x except 0 return 0; 0 return 1. We can use the property that every number(except 0) bitOr its negative number is 0xFFFFFFFF
+  return ((x | (~x + 1)) >> 31) + 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
